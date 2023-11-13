eafdb771803e4f1bf5f127f85aee5e643ac55235
Abdulbarikt <bariponmala121@gmail.com>
2023-11-13 16:09:08 +0530

    justedit

--- lib/Screens/create.dart
+++ lib/Screens/create.dart
@@ -11,7 +11,7 @@ class Create extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: SingleChildScrollView  (
+        body: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
